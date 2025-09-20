bc2eacfe87b656245c979e350105b4b4b34f6a76
veerbal1 <veerbalsingh1@gmail.com>
2025-09-20 15:24:47 +0530

    Refactor Account structure to use type aliases for ID and balance; implement deposit and withdraw methods with error handling and test cases in main function.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+type TAccountID = i32;
+type TBalanace = f64;
+
 #[derive(Debug)]
 struct Account {
-    id: i32,
-    balance: f64,
+    id: TAccountID,
+    balance: TBalanace,
 }
 
 struct Bank {
@@ -17,13 +20,51 @@ impl Bank {
         }
     }
 
-    fn create_account(&mut self, id: i32, initial_balance: f64) {
+    fn create_account(&mut self, id: TAccountID, initial_balance: TBalanace) {
         let account = Account {
             id,
             balance: initial_balance,
         };
 
         self.accounts.push(account);
+    }
+
+    fn deposit(&mut self, id: TAccountID, amount: TBalanace) -> Result<String, String> {
+        if amount < 0.0 {
+            return Err(String::from("Amount must be Positive"));
+        }
+
+        for account in self.accounts.iter_mut() {
+            if account.id == id {
+                account.balance += amount;
+                return Ok(format!(
+                    "Deposit successfully: New Balance: {}",
+                    account.balance
+                ));
+            }
+        }
+        Err(String::from("Account not found"))
+    }
+
+    fn withdraw(&mut self, id: TAccountID, amount: TBalanace) -> Result<String, String> {
+        if amount < 0.0 {
+            return Err(String::from("Amount must be positive!"));
+        }
+
+        for account in self.accounts.iter_mut() {
+            if account.id == id {
+                if account.balance < amount {
+                    return Err(String::from("Insufficent Balance"));
+                } else {
+                    account.balance -= amount;
+                    return Ok(format!(
+                        "Withdraw successfully: New Balance: {}",
+                        account.balance
+                    ));
+                }
+            }
+        }
+        Err(String::from("Account not found!"))
     }
 }
 
@@ -33,6 +74,27 @@ fn main() {
     new_bank.create_account(1, 1000.0);
     new_bank.create_account(2, 500.0);
 
+    for (_, account) in new_bank.accounts.iter().enumerate() {
+        println!(
+            "Account: \n
+                ID: {}\n
+                Balance: {}",
+            account.id, account.balance
+        )
+    }
+
+    // Test deposit:
+    match new_bank.deposit(1, 100.0) {
+        Ok(val) => println!("{}", val),
+        Err(e) => println!("{}", e),
+    }
+
+    match new_bank.withdraw(2, 50.0) {
+        Ok(val) => println!("{}", val),
+        Err(e) => println!("{}", e),
+    }
+
+    println!("After Transactions");
     for (_, account) in new_bank.accounts.iter().enumerate() {
         println!(
             "Account: \n
