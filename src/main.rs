7904ab9e78b60235c9d71af1647692dd72cc9833
veerbal1 <veerbalsingh1@gmail.com>
2025-09-20 15:03:20 +0530

    Add Bank and Account structures with account creation functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+#[derive(Debug)]
+struct Account {
+    id: i32,
+    balance: f64,
+}
+
+struct Bank {
+    name: String,
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new(bank_name: String) -> Self {
+        Bank {
+            name: bank_name,
+            accounts: Vec::new(),
+        }
+    }
+
+    fn create_account(&mut self, id: i32, initial_balance: f64) {
+        let account = Account {
+            id,
+            balance: initial_balance,
+        };
+
+        self.accounts.push(account);
+    }
+}
+
 fn main() {
-    println!("Welcome to Rust Bank!")
+    let mut new_bank = Bank::new(String::from("Punjab National Bank"));
+    println!("Bank has been created! {}", new_bank.name);
+    new_bank.create_account(1, 1000.0);
+    new_bank.create_account(2, 500.0);
+
+    for (_, account) in new_bank.accounts.iter().enumerate() {
+        println!(
+            "Account: \n
+                ID: {}\n
+                Balance: {}",
+            account.id, account.balance
+        )
+    }
 }
