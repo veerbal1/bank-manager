ba0c416c75ffbdbc9fc7cea27147174f4eeeba97
veerbal1 <veerbalsingh1@gmail.com>
2025-09-20 17:27:09 +0530

    Add transaction history to Account structure; implement methods to retrieve balance and transaction history, and update main function to demonstrate functionality.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ type TBalanace = f64;
 struct Account {
     id: TAccountID,
     balance: TBalanace,
+    transaction_history: Vec<String>,
 }
 
 struct Bank {
@@ -24,6 +25,7 @@ impl Bank {
         let account = Account {
             id,
             balance: initial_balance,
+            transaction_history: Vec::new(),
         };
 
         self.accounts.push(account);
@@ -37,6 +39,9 @@ impl Bank {
         for account in self.accounts.iter_mut() {
             if account.id == id {
                 account.balance += amount;
+                account
+                    .transaction_history
+                    .push(String::from(format!("Deposited {}", amount)));
                 return Ok(format!(
                     "Deposit successfully: New Balance: {}",
                     account.balance
@@ -57,6 +62,9 @@ impl Bank {
                     return Err(String::from("Insufficent Balance"));
                 } else {
                     account.balance -= amount;
+                    account
+                        .transaction_history
+                        .push(format!("Withdrew {}", amount));
                     return Ok(format!(
                         "Withdraw successfully: New Balance: {}",
                         account.balance
@@ -65,6 +73,24 @@ impl Bank {
             }
         }
         Err(String::from("Account not found!"))
+    }
+
+    fn get_balance(&self, id: TAccountID) -> Result<TBalanace, String> {
+        for account in self.accounts.iter() {
+            if account.id == id {
+                return Ok(account.balance);
+            }
+        }
+        Err(String::from("Account not found"))
+    }
+
+    fn get_transaction_history(&self, id: TAccountID) -> Result<&Vec<String>, String> {
+        for account in self.accounts.iter() {
+            if account.id == id {
+                return Ok(&account.transaction_history);
+            }
+        }
+        Err(String::from("Account not Found"))
     }
 }
 
@@ -88,6 +114,14 @@ fn main() {
         Ok(val) => println!("{}", val),
         Err(e) => println!("{}", e),
     }
+    match new_bank.deposit(1, 149.0) {
+        Ok(val) => println!("{}", val),
+        Err(e) => println!("{}", e),
+    }
+    match new_bank.deposit(1, 159.0) {
+        Ok(val) => println!("{}", val),
+        Err(e) => println!("{}", e),
+    }
 
     match new_bank.withdraw(2, 50.0) {
         Ok(val) => println!("{}", val),
@@ -102,5 +136,24 @@ fn main() {
                 Balance: {}",
             account.id, account.balance
         )
+    }
+
+    println!("Test balance");
+
+    let balance = new_bank.get_balance(1);
+    match balance {
+        Ok(val) => println!("Balance is: {}", val),
+        Err(e) => println!("Error: {}", e),
+    }
+
+    println!("Get Transaction History");
+    let transaction_history = new_bank.get_transaction_history(1);
+    match transaction_history {
+        Ok(val) => {
+            for transaction in val.iter() {
+                println!("{}", transaction)
+            }
+        },
+        Err(e) => println!("Error getting transaction history: {}", e),
     }
 }
